1d9f515bbf6f1d5b7e7ab0ed84da97921e98b22c
LUCSALE <luciano@lucsale.com.br>
2026-08-20 15:58:26 -0300

    Posição Atual

---
+++ FrmMoviRetiradaMovimentacao.Designer.cs
@@ -0,0 +1,259 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace HELP_Princ
+{
+    partial class FrmMoviRetiradaMovimentacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMoviRetiradaMovimentacao));
+            this.pnlHeader = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.lblTitulo = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.pnlGrid = new Guna.UI2.WinForms.Guna2Panel();
+            this.mOVI_RETIRADABindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.helpdesk01DataSet = new HELP_Princ.helpdesk01DataSet();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.tmiEfeitos = new System.Windows.Forms.Timer(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.mOVIBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.mOVI_RETIRADATableAdapter = new HELP_Princ.helpdesk01DataSetTableAdapters.MOVI_RETIRADATableAdapter();
+            this.tECNICOSBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tECNICOSTableAdapter = new HELP_Princ.helpdesk01DataSetTableAdapters.TECNICOSTableAdapter();
+            this.mOVITableAdapter = new HELP_Princ.helpdesk01DataSetTableAdapters.MOVITableAdapter();
+            this.eQUIPAMENTOSBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.eQUIPAMENTOSTableAdapter = new HELP_Princ.helpdesk01DataSetTableAdapters.EQUIPAMENTOSTableAdapter();
+            this.pnlHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mOVI_RETIRADABindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.helpdesk01DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mOVIBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tECNICOSBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eQUIPAMENTOSBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BorderColor = System.Drawing.Color.SlateGray;
+            this.pnlHeader.BorderRadius = 10;
+            this.pnlHeader.BorderThickness = 1;
+            this.pnlHeader.Controls.Add(this.guna2HtmlLabel3);
+            this.pnlHeader.Controls.Add(this.guna2ControlBox1);
+            this.pnlHeader.Controls.Add(this.guna2PictureBox2);
+            this.pnlHeader.Controls.Add(this.lblTitulo);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 2);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Padding = new System.Windows.Forms.Padding(0, 2, 0, 0);
+            this.pnlHeader.Size = new System.Drawing.Size(687, 112);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Segoe UI Semilight", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.SlateGray;
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(304, 64);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(79, 15);
+            this.guna2HtmlLabel3.TabIndex = 6;
+            this.guna2HtmlLabel3.TabStop = false;
+            this.guna2HtmlLabel3.Text = "(Movimentação)";
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.Animated = true;
+            this.guna2ControlBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.guna2ControlBox1.BorderColor = System.Drawing.Color.Silver;
+            this.guna2ControlBox1.BorderRadius = 10;
+            this.guna2ControlBox1.BorderThickness = 1;
+            this.guna2ControlBox1.CausesValidation = false;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.LightGray;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.DimGray;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(658, 5);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(20, 20);
+            this.guna2ControlBox1.TabIndex = 4;
+            this.guna2ControlBox1.TabStop = false;
+            this.guna2ControlBox1.Click += new System.EventHandler(this.guna2ControlBox1_Click);
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.guna2PictureBox2.FillColor = System.Drawing.SystemColors.Window;
+            this.guna2PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox2.Image")));
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(13, 10);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.ShadowDecoration.Color = System.Drawing.Color.DimGray;
+            this.guna2PictureBox2.ShadowDecoration.Enabled = true;
+            this.guna2PictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.guna2PictureBox2.TabIndex = 2;
+            this.guna2PictureBox2.TabStop = false;
+            this.guna2PictureBox2.UseTransparentBackground = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.SlateGray;
+            this.lblTitulo.Location = new System.Drawing.Point(193, 37);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(300, 31);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.TabStop = false;
+            this.lblTitulo.Text = "Retirada de Equipamento";
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.BorderColor = System.Drawing.Color.SlateGray;
+            this.pnlGrid.BorderRadius = 10;
+            this.pnlGrid.BorderThickness = 1;
+            this.pnlGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGrid.Location = new System.Drawing.Point(0, 114);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(687, 192);
+            this.pnlGrid.TabIndex = 4;
+            // 
+            // mOVI_RETIRADABindingSource
+            // 
+            this.mOVI_RETIRADABindingSource.DataMember = "MOVI_RETIRADA";
+            this.mOVI_RETIRADABindingSource.DataSource = this.helpdesk01DataSet;
+            // 
+            // helpdesk01DataSet
+            // 
+            this.helpdesk01DataSet.DataSetName = "helpdesk01DataSet";
+            this.helpdesk01DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.AnimateWindow = true;
+            this.guna2BorderlessForm1.AnimationType = Guna.UI2.WinForms.Guna2BorderlessForm.AnimateWindowType.AW_CENTER;
+            this.guna2BorderlessForm1.BorderRadius = 15;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // tmiEfeitos
+            // 
+            this.tmiEfeitos.Interval = 700;
+            this.tmiEfeitos.Tick += new System.EventHandler(this.tmiEfeitos_Tick);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // mOVIBindingSource
+            // 
+            this.mOVIBindingSource.DataMember = "MOVI";
+            this.mOVIBindingSource.DataSource = this.helpdesk01DataSet;
+            // 
+            // mOVI_RETIRADATableAdapter
+            // 
+            this.mOVI_RETIRADATableAdapter.ClearBeforeFill = true;
+            // 
+            // tECNICOSBindingSource
+            // 
+            this.tECNICOSBindingSource.DataMember = "TECNICOS";
+            this.tECNICOSBindingSource.DataSource = this.helpdesk01DataSet;
+            // 
+            // tECNICOSTableAdapter
+            // 
+            this.tECNICOSTableAdapter.ClearBeforeFill = true;
+            // 
+            // mOVITableAdapter
+            // 
+            this.mOVITableAdapter.ClearBeforeFill = true;
+            // 
+            // eQUIPAMENTOSBindingSource
+            // 
+            this.eQUIPAMENTOSBindingSource.DataMember = "EQUIPAMENTOS";
+            this.eQUIPAMENTOSBindingSource.DataSource = this.helpdesk01DataSet;
+            // 
+            // eQUIPAMENTOSTableAdapter
+            // 
+            this.eQUIPAMENTOSTableAdapter.ClearBeforeFill = true;
+            // 
+            // FrmMoviRetiradaMovimentacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(687, 306);
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.pnlHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmMoviRetiradaMovimentacao";
+            this.Padding = new System.Windows.Forms.Padding(0, 2, 0, 0);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmMoviRetiradaMovimentacao";
+            this.Activated += new System.EventHandler(this.FrmMoviRetiradaMovimentacao_Activated);
+            this.Load += new System.EventHandler(this.FrmMoviRetiradaMovimentacao_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmMoviRetiradaMovimentacao_KeyDown);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mOVI_RETIRADABindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.helpdesk01DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mOVIBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tECNICOSBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eQUIPAMENTOSBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel pnlHeader;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblTitulo;
+        private Guna.UI2.WinForms.Guna2Panel pnlGrid;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private helpdesk01DataSet helpdesk01DataSet;
+        private Timer tmiEfeitos;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private BindingSource mOVI_RETIRADABindingSource;
+        private helpdesk01DataSetTableAdapters.MOVI_RETIRADATableAdapter mOVI_RETIRADATableAdapter;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private ErrorProvider errorProvider1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private BindingSource tECNICOSBindingSource;
+        private helpdesk01DataSetTableAdapters.TECNICOSTableAdapter tECNICOSTableAdapter;
+        private BindingSource mOVIBindingSource;
+        private helpdesk01DataSetTableAdapters.MOVITableAdapter mOVITableAdapter;
+        private BindingSource eQUIPAMENTOSBindingSource;
+        private helpdesk01DataSetTableAdapters.EQUIPAMENTOSTableAdapter eQUIPAMENTOSTableAdapter;
+    }
+}
---
+++ FrmMoviRetiradaMovimentacao.cs
@@ -0,0 +1,222 @@
+﻿                      using System;
+using System.Drawing; // Necessário para Color
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Controls.Primitives;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics.Eventing.Reader;
+using Guna.UI2.WinForms;
+
+namespace HELP_Princ
+{
+    public partial class FrmMoviRetiradaMovimentacao : Form
+    {
+        public FrmMoviRetiradaMovimentacao()
+        {
+            InitializeComponent();
+
+            this.AutoScroll = true; // Habilita a barra de rolagem automática
+            //fcnConfigCampos(); //Conifigura campos para navegação com a tecla Enter e Destaque do campo ao entrar e restaura a cor ao sair
+        }
+
+
+        private void FrmMoviRetiradaMovimentacao_Load(object sender, EventArgs e)
+        {
+            tmiEfeitos.Start();
+
+            //this.mOVI_RETIRADATableAdapter.FillByID(this.helpdesk01DataSet.MOVI_RETIRADA, InfoPesq.ID);
+            
+
+        }
+
+
+        private void pnlFooter_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dbgTecnicos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void fillBy1ToolStripButton_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void fillByToolStripButton_Click_1(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void fillByToolStripButton_Click_2(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void dbgUsuarios_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salvarToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPesquisarUsuario_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtESTAGIO_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmiEfeitos_Tick(object sender, EventArgs e)
+        {
+            // Alterna entre Vermelho e a cor SlateGray
+            lblTitulo.ForeColor = (lblTitulo.ForeColor == Color.SlateGray) ? Color.Red : Color.SlateGray;
+
+
+            // Opcional: Alternar a visibilidade em vez da cor
+            // label1.Visible = !label1.Visible;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void FrmMoviRetiradaMovimentacao_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+
+        private void txtDESCRICA_ATIVIDADE_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+
+        private void FrmMoviRetiradaMovimentacao_Activated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDATA_PREVISTA_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void txtDESCRICA_ATIVIDADE_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void guna2ControlBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbtAgendamento_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbSimples_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbSimples_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbSubstituicao_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbtManutencao_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void cbtnNovoTecnico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void cbtnNovoEquipamento_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void txtPATRIMONIO_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void txtSAI_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void cbxTecnico_Atuante_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+        
+
+}
+ 
